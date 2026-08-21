f9bb70a639e97ba472aebbff532ccf23ad505a10
avinash752 <avinashmallesh752@gmail.com>
2026-08-21 06:46:46 +0530

    Update BikeStores Sample Database - drop all objects.sql

--- BikeStores Sample Database - drop all objects.sql
+++ BikeStores Sample Database - drop all objects.sql
@@ -1,13 +1,3 @@
-/*
---------------------------------------------------------------------
-Last Edited: March 29, 2024 
---------------------------------------------------------------------
-Name   : BikeStores
-Link   : https://github.com/ShaidHasanShuvo/Bike_Stores
-Version: 1.0
---------------------------------------------------------------------
-*/
-
 -- drop tables
 DROP TABLE IF EXISTS sales.order_items;
 DROP TABLE IF EXISTS sales.orders;
